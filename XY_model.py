c20e74c7a036e2064a738961057003956325e5b2
MW <marcin119a@gmail.com>
2022-03-23 10:43:41 +0100

    refractor + spins as thetas

--- XY_model.py
+++ XY_model.py
@@ -1,42 +1,39 @@
-## IMPORT LIBRARY
 import numpy as np
-import scipy as sp
-import matplotlib as mpl
 import matplotlib.pyplot as plt
-import pylab
 import random
-import matplotlib.image as mpimg
-from matplotlib.legend_handler import HandlerLine2D
-import math
-from scipy.optimize import curve_fit
 from numpy import pi
 from scipy.stats import gamma
 from mpl_toolkits.axes_grid1 import make_axes_locatable
-from scipy.stats import truncnorm
+import random
 
-
-## applying Metropolis algorithm
-# input: T/temperature
-#        S/spins configuration(in 1d list)
-#        H/ecternal field.default value=0
+"""
+ applying Metropolis algorithm
+ input: T/temperature
+        S/spins configuration(in 1d list)
+        H/ecternal field.default value=0
+"""
 
 class XYSystem():
-    def __init__(self,temperature = 3,width=10):
+    def __init__(self, temperature = 3, width=10):
         self.width = width
         self.num_spins = width**2
-        L,N = self.width,self.num_spins
+        L, N = self.width, self.num_spins
         self.nbr = {i : ((i // L) * L + (i + 1) % L, (i + L) % N,
                     (i // L) * L + (i - 1) % L, (i - L) % N) \
                                             for i in list(range(N))}
-        self.spin_config = gamma.rvs(np.ones(self.num_spins))
+        self.thetas = gamma.rvs(np.ones(self.num_spins))
+        self.bound = 30
+        self.spin_config = self._transf(self.thetas, -pi, pi, 0, self.bound)
         self.temperature = temperature
         self.energy = np.sum(self.get_energy())/self.num_spins
         self.M = []
         self.Cv = []
 
+    def _transf(self, t, c, d, a, b):
+        return c+ ((d-c)/(b-a)) * (t - a)
 
-    def set_temperature(self,temperature):
-        self.temperature = temperature;
+    def set_temperature(self, temperature):
+        self.temperature = temperature
     
     def sweep(self):
         beta = 1.0 / self.temperature
@@ -44,19 +41,21 @@ class XYSystem():
         random.shuffle(spin_idx)
         for idx in spin_idx:#one sweep in defined as N attempts of flip
             #k = np.random.randint(0, N - 1)#randomly choose a spin
-            energy_i = -sum(np.cos(self.spin_config[idx]-self.spin_config[n]) for n in self.nbr[idx]) 
-            dtheta =  truncnorm.pdf(self.spin_config[idx], 0, 1)
-            spin_temp = self.spin_config[idx] + dtheta
+            energy_i = -sum(np.cos(self.spin_config[idx]-self.spin_config[n]) for n in self.nbr[idx])
+            d_theta = np.random.uniform(-pi, pi)
+            spin_temp = self.spin_config[idx] + d_theta
             energy_f = -sum(np.cos(spin_temp-self.spin_config[n]) for n in self.nbr[idx]) 
             delta_E = energy_f - energy_i
             if np.random.uniform(0.0, 1.0) < np.exp(-beta * delta_E):
-                self.spin_config[idx] += dtheta
+                self.spin_config[idx] += d_theta
 
 
 
-    ## calculate the energy of a given configuration  
-    #  input: S/spin configuration in list
-    #         H/external field, defult 0
+    """ 
+    calculate the energy of a given configuration  
+    input: S/spin configuration in list
+             H/external field, defult 0
+    """
     def get_energy(self):
         energy_=np.zeros(np.shape(self.spin_config))
         idx = 0
@@ -74,19 +73,19 @@ class XYSystem():
         beta = 1.0/self.temperature
         energy_temp = 0
         for k in list(range(max_nsweeps)):
-            self.sweep()     
-            #list_M.append(np.abs(np.sum(S)/N))
+            self.sweep()
             energy = np.sum(self.get_energy())/self.num_spins/2
             dic_thermal_t['energy'] += [energy]
-            #print( abs(energy-energy_temp)/abs(energy))
             if show  & (k%1e3 ==0):
                 print('#sweeps=%i'% (k+1))
                 print('energy=%.2f'%energy)
-                self.show()
+                self.show_map()
             if ((abs(energy-energy_temp)/abs(energy)<1e-4) & (k>500)) or k == max_nsweeps-1:
                 print('\nequilibrium state is reached at T=%.1f'%self.temperature)
                 print('#sweep=%i'%k)
                 print('energy=%.2f'%energy)
+                if show:
+                    self.show_map()
                 break
             energy_temp = energy
         nstates = len(dic_thermal_t['energy'])
@@ -95,11 +94,21 @@ class XYSystem():
         energy2=np.average(np.power(dic_thermal_t['energy'][int(nstates/2):],2))
         self.Cv=(energy2-energy**2)*beta**2
 
-    ## To see thermoquantities evolve as we cooling the systems down
-    # input: T_inital: initial tempreature
-    #        T_final: final temperature
-    #        sample/'log' or 'lin',mean linear sampled T or log sampled( centered at critical point)
-    def annealing(self,T_init=2.5,T_final=0.1,nsteps = 20,show_equi=False):
+    def get_thetas(self, degree=False):
+        config_matrix = self.list2matrix(self.spin_config)
+        x = np.cos(self.spin_config)
+        y = np.sin(self.spin_config)
+        thetas = np.arctan2(y, x)
+
+        return thetas
+
+    """
+    To see thermoquantities evolve as we cooling the systems down
+    input: T_inital: initial tempreature
+           T_final: final temperature
+           sample/'log' or 'lin',mean linear sampled T or log sampled( centered at critical point)
+    """
+    def annealing(self, T_init=2.5, T_final=0.1, nsteps = 20, show_equi=False):
         # initialize spins. Orientations are taken from 0 - 2pi randomly.
         #initialize spin configuration  
         dic_thermal = {}
@@ -109,7 +118,7 @@ class XYSystem():
         for T in dic_thermal['temperature']:
             self.equilibrate(temperature=T)
             if show_equi:
-                self.show()
+                self.show_map()
             dic_thermal['energy'] += [self.energy]
             dic_thermal['Cv'] += [self.Cv]
         plt.plot(dic_thermal['temperature'],dic_thermal['Cv'],'.')
@@ -122,25 +131,28 @@ class XYSystem():
         plt.show()
         return dic_thermal
 
+    """
+    convert configuration inz list to matrix form
+    """
     @staticmethod
-    ## convert configuration inz list to matrix form
-    def list2matrix(S):
-        N=int(np.size(S))
-        L = int(np.sqrt(N))
-        S=np.reshape(S,(L,L))
-        return S
+    def list2matrix(s):
+        n = int(np.size(s))
+        l = int(np.sqrt(n))
+        s = np.reshape(s, (l, l))
+        return s
 
-    ## visulize a configurtion
-    #  input：S/ spin configuration in list form
+    """
+    visulize a configurtion
+    input：S/ spin configuration in list form
+    """
     def show(self, colored=False):
         config_matrix = self.list2matrix(self.spin_config)
-        X, Y = np.meshgrid(np.arange(0,self.width ),np.arange(0, self.width))
-        U = np.cos(config_matrix )
-        V = np.sin(config_matrix )
+        x, y = np.meshgrid(np.arange(0,self.width ),np.arange(0, self.width))
+        u = np.cos(config_matrix )
+        v = np.sin(config_matrix )
         plt.figure(figsize=(4,4), dpi=100)
-        Q = plt.quiver(X, Y, U, V, units='width')
-        qk = plt.quiverkey(Q, 0.1, 0.1, 1, r'$spin$', labelpos='E',
-                    coordinates='figure')
+        Q = plt.quiver(x, y, u, v, units='width')
+        plt.quiverkey(Q, 0.1, 0.1, 1, r'$spin$', labelpos='E',  coordinates='figure')
         plt.title('T=%.2f'%self.temperature+', #spins='+str(self.width)+'x'+str(self.width))
         plt.axis('off')
         plt.show()
@@ -154,5 +166,4 @@ class XYSystem():
         cax0 = divider0.append_axes("right", size="10%", pad=0.05)
 
         fig.colorbar(im0, cax=cax0)
-        import random
         plt.savefig(f'{random.randint(1,100)}.png')
